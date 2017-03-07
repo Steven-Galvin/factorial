45e6ce8b80b33a4750d74cbe2198246e0c72aa20
Monique St Laurent and Steven Galvin <monique@>
2017-03-06 20:57:40 -0800

    seperate back and front end logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,29 @@
-$(document).ready(function(){
-  $("form").submit(function(event){
-  var number = parseInt($("#userNumber").val());
+// $(document).ready(function(){
+//   $("form").submit(function(event){
+//   var number = parseInt($("#userNumber").val());
 
-  var factorial = [];
-
-  for (var index = 1; index <= number; index+= 1) {
-  factorial.push(index);
+  var factorial = function(userNum) {
+    var factorialArray = [];
+    for (var index = 1; index <= userNum; index+= 1) {
+      factorialArray.push(index);
+    };
+    console.log(factorialArray);
+    var total = 1
+    for (var index = 0; index < factorialArray.length; index += 1) {
+      total *= factorialArray[index];
+    }
+    return total;
   };
 
-  var total = 1
-  for (var index = 0; index < factorial.length; index += 1) {
-  total *= factorial[index];
-  }
-
-
-  $("#number").text(number);
-  $("#total").text(total);
-  $(".result").show();
+  $(document).ready(function(){
+    $("form").submit(function(event){
+      var number = parseInt($("#userNumber").val());
+      // console.log(number);
+      var result = factorial(number);
+      $("#number").text(number);
+      $("#total").text(result);
+      $(".result").show();
   // console.log(total);
-  event.preventDefault();
+      event.preventDefault();
 });
 });
